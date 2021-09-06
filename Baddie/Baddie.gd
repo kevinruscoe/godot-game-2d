567df79791167497dbfaf3110c26da836dd5ee3e
Kevin Ruscoe <kevin@popcreative.co.uk>
2021-09-06 13:58:04 +0100

    things

--- Baddie/Baddie.gd
+++ Baddie/Baddie.gd
@@ -8,6 +8,12 @@ var start_position = Vector2()
 var velocity = Vector2()
 var going_right = false
 
+onready var raycast_up = get_node("RayCasts/UpRayCast2D")
+onready var raycast_left = get_node("RayCasts/LeftRayCast2D")
+onready var raycast_right = get_node("RayCasts/RightRayCast2D")
+
+signal hurt_player
+
 func _ready():
 	start_position = position
 
@@ -24,5 +30,12 @@ func _physics_process(delta):
 		
 	if position.x < start_position.x - distance_allowed_to_move:
 		going_right = true
+
+	for i in range(get_slide_count()):
+		var collision = get_slide_collision(i)
+		
+		if collision.collider is KinematicBody2D:
+			# TODO: work out if its actually the player
+			emit_signal("hurt_player")
 		
 	velocity = move_and_slide(velocity, Vector2(0, -1))
--- Player/Player.gd
+++ Player/Player.gd
@@ -1,5 +1,7 @@
 extends KinematicBody2D
 
+var is_player = true
+
 var run_speed = 100
 var jump_speed = 400
 var gravity = 1200
@@ -14,7 +16,9 @@ var pressing_right = false
 var pressed_space = false
 
 onready var sprite = get_node("AnimatedSprite")
-onready var up_raycast = get_node("UpRayCast2D")
+onready var raycast_up = get_node("RayCasts/UpRayCast2D")
+onready var raycast_left = get_node("RayCasts/LeftRayCast2D")
+onready var raycast_right = get_node("RayCasts/RightRayCast2D")
 onready var camera = get_node("Camera2D")
 onready var time_label = camera.get_node("Time");
 
@@ -62,16 +66,9 @@ func handle_collisions():
 
 	for i in range(get_slide_count()):
 		var collision = get_slide_collision(i)
-		
-		if collision.collider is KinematicBody2D:
-			handle_enemy_collision(collision)
 
 		if collision.collider is TileMap:
 			handle_tilemap_collision(collision)
-
-
-func handle_enemy_collision(_collision):
-	sprite.set_modulate(Color8(255, 0, 0, 45))
 
 
 func handle_tilemap_collision(collision):
@@ -79,9 +76,9 @@ func handle_tilemap_collision(collision):
 	# Find the tile we've collided with
 	var tile_pos = collision.collider.world_to_map(collision.position - collision.normal)
 	var tile_name = collision.collider.tile_set.tile_get_name(collision.collider.get_cellv(tile_pos))
-		
+			
 	# hitting blocks from below
-	if is_jumping && up_raycast.get_collider():
+	if is_jumping && raycast_up.get_collider():
 		if tile_name == "pickup":
 			collision.collider.set_cellv(tile_pos, collision.collider.tile_set.find_tile_by_name("pickup_used"))
 
@@ -106,3 +103,7 @@ func _physics_process(delta):
 	# toggle jump
 	if is_on_floor() && is_jumping:
 		is_jumping = false
+
+
+func _on_hurt_player():
+	print("ow")
